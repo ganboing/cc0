c6c1b6cf373b4962f737740d5c24e2813689ff8a
ganboing <ganboing@gmail.com>
2014-06-05 23:13:20 +0800

    Signed-off-by: ganboing <ganboing@gmail.com>

---
+++ set-env.cmd
@@ -0,0 +1,4 @@
+set INCLUDE=C:\Users\PROGMAN\workspace\libantlr3c-3.4\include
+set LIB=C:\Users\PROGMAN\workspace\libantlr3c-3.4\x64\Release
+path C:\Users\PROGMAN\workspace\antlr-3.4;C:\Users\PROGMAN\workspace\mcpp-2.7.2\bin;%path%
+cmd
