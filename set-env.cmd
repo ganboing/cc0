4726bc36ac6d3428aaf4ab7bd2b938a123934365
ganboing <ganboing@gmail.com>
2014-06-07 00:40:30 +0800

    modify startup batch script

--- set-env.cmd
+++ set-env.cmd
@@ -1,4 +1,3 @@
 set INCLUDE=C:\Users\PROGMAN\workspace\libantlr3c-3.4\include
 set LIB=C:\Users\PROGMAN\workspace\libantlr3c-3.4\x64\Release
-path C:\Users\PROGMAN\workspace\antlr-3.4;C:\Users\PROGMAN\workspace\mcpp-2.7.2\bin;%path%
 cmd
---
+++ start-vc.cmd
@@ -0,0 +1,3 @@
+path C:\Users\PROGMAN\workspace\antlr-3.4;C:\Users\PROGMAN\workspace\mcpp-2.7.2\bin;%path%
+"%programfiles(x86)%\Microsoft Visual Studio 12.0\Common7\IDE\devenv.exe"
+cmd
