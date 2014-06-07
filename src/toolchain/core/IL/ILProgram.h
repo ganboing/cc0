26f098fe44414885dfe68c6815f1566fde1a8590
progman <progman@ubuntu.(none)>
2014-06-07 17:14:20 +0800

    working on serialization

--- src/toolchain/core/IL/ILProgram.h
+++ src/toolchain/core/IL/ILProgram.h
@@ -1,12 +1,23 @@
 #pragma once 
 
 #include <vector>
+#include <boost/serialization/access.hpp>
+#include <boost/serialization/nvp.hpp>
+#include <boost/serialization/vector.hpp>
 
 class ILClass;
 class SymbolScope;
 
 class ILProgram
 {
+private:
+	friend ::boost::serialization::access;
+	template<class Archive>
+	void serialize(Archive& ar, const unsigned int ver)
+	{
+		ar & Claases;
+		ar & Scope;
+	}
 public:
     std::vector<ILClass *> Claases;
     SymbolScope *Scope;
