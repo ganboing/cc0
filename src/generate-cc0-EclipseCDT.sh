5ffca3d88609b3ca862c83f2c8b7f20b3c1bdcf7
progman <progman@ubuntu.(none)>
2014-06-11 14:31:42 +0800

    build script modified

--- src/generate-cc0-EclipseCDT.sh
+++ src/generate-cc0-EclipseCDT.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
-dir=$(dirname "$0")
+dir=$(cd $(dirname "$0"); pwd)
+cwd=$(pwd)
 
-mkdir -p $dir/../../cc0-EclipseCDT
+echo "running from ""$dir"
 
-cd $dir/../../cc0-EclipseCDT
+mkdir -p $cwd/cc0-build
+
+echo "generating to "$cwd"/cc0-EclipseCDT"
+
+cd $cwd/cc0-EclipseCDT
 
 # Accept an optional command line parameter to set the build type.
 # Valid build types are Debug, Release, RelWithDebInfo, MinSizeRel
 if [ -z "$1" ]; then
-	cmake -DCMAKE_INSTALL_PREFIX:PATH=$dir/../cc0-install -G"Eclipse CDT4 - Unix Makefiles" -DCMAKE_BUILD_TYPE=$1 -DCC0_USE_STATIC_CRT=1 $dir/../cc0/src/toolchain/
+	cmake -DCMAKE_INSTALL_PREFIX:PATH=$cwd/cc0-install -G"Eclipse CDT4 - Unix Makefiles" -DCMAKE_BUILD_TYPE=$1 -DCC0_USE_STATIC_CRT=1 $dir/toolchain/
 else
-	cmake -DCMAKE_INSTALL_PREFIX:PATH=$dir/../cc0-install -G"Eclipse CDT4 - Unix Makefiles" -DCC0_USE_STATIC_CRT=1 $dir/../cc0/src/toolchain
+	cmake -DCMAKE_INSTALL_PREFIX:PATH=$cwd/cc0-install -G"Eclipse CDT4 - Unix Makefiles" -DCC0_USE_STATIC_CRT=1 $dir/toolchain/
 fi
 
 # make VERBOSE=1
--- src/generate-cc0-dev.sh
+++ src/generate-cc0-dev.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
-dir=$(dirname "$0")
+dir=$(cd $(dirname "$0"); pwd)
+cwd=$(pwd)
 
-mkdir -p $dir/../../cc0-build
+echo "running from ""$dir"
 
-cd $dir/../../cc0-build
+mkdir -p $cwd/cc0-build
+
+echo "generating to "$cwd"/cc0-build"
+
+cd $cwd/cc0-build
 
 # Accept an optional command line parameter to set the build type.
 # Valid build types are Debug, Release, RelWithDebInfo, MinSizeRel
 if [ -z "$1" ]; then
-	cmake -DCMAKE_INSTALL_PREFIX:PATH=$dir/../cc0-install -DCMAKE_BUILD_TYPE=$1 -DCC0_USE_STATIC_CRT=1 $dir/../cc0/src/toolchain/
+	cmake -DCMAKE_INSTALL_PREFIX:PATH=$cwd/cc0-install -DCMAKE_BUILD_TYPE=$1 -DCC0_USE_STATIC_CRT=1 $dir/toolchain/
 else
-	cmake -DCMAKE_INSTALL_PREFIX:PATH=$dir/../cc0-install -DCC0_USE_STATIC_CRT=1 $dir/../cc0/src/toolchain
+	cmake -DCMAKE_INSTALL_PREFIX:PATH=$cwd/cc0-install -DCC0_USE_STATIC_CRT=1 $dir/toolchain/
 fi
 
 # make VERBOSE=1
