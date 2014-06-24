fe4e6ca831ad77e4ddcbb9fe26c836f1d5c6d30a
progman <progman@ubuntu.(none)>
2014-06-25 03:10:05 +0800

    fix some small bug to silence the compiler

--- src/toolchain/core/IL/IL.cpp
+++ src/toolchain/core/IL/IL.cpp
@@ -3,6 +3,7 @@
 #include <core/Core.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdint.h>
 #include <assert.h>
 
 IL::IL():Opcode(Nop)
@@ -222,7 +223,7 @@ std::string IL::GetOperandString(const IL::ILOperand& operand)
             opstr += buffer;
             break;
         case Variable:
-            sprintf(buffer, "%s$%#llx", operand.SymRef->Name.c_str(), operand.SymRef->Scope);
+            sprintf(buffer, "%s$%p", operand.SymRef->Name.c_str(), operand.SymRef->Scope);
             opstr += buffer;
             break;
         case FieldToken:
